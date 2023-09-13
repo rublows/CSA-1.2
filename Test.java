f08ae28e0c17a8122d1079314cdba6572bd2a51c
rublows <144463583+rublows@users.noreply.github.com>
2023-09-13 00:08:14 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+  public static void main(String[] args) {
+    int num1 = 12;
+    double num2 = 2.50;
+    System.out.print("What is 12 + 2.50");
+    System.out.println(num1 - num2);
+    boolean X = false;
+    System.out.println(X);
+    System.out.println(num1 + num2);
+    boolean Y = true;
+    System.out.println(Y);
+  }
+}
